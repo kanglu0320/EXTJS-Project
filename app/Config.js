741b342dcb641a453a56e1b3a83210e1d3c45490
kanglu0320 <53844348+kanglu0320@users.noreply.github.com>
2021-05-20 12:04:16 -0400

    Update Config.js

--- app/Config.js
+++ app/Config.js
@@ -14,18 +14,15 @@
 
 
     getReportServiceUrl: function () {
-        //return 'http://vm-city102:6080/arcgis/rest/services/DelDOT_NPDES_Reports/makeReport/GPServer/Make%20Report';
-        return 'https://newarkgis.kci.com/arcgis/rest/services/DelDOT_NPDES_reports/makeReport/GPServer/Make%20Report';
+        return 'http://localhost:6080/arcgis/rest/services/DelDOT_NPDES_Reports/makeReport/GPServer/Make%20Report';
     },
 
     getPrintMapUrl: function () {
-        //return 'http://vm-deldot103:6080/arcgis/rest/services/DelDotWebViewer/ExportWebMapTest/GPServer/Export%20Web%20Map';
-        return 'https://newarkgis.kci.com/arcgis/rest/services/DelDotWebViewer/ExportWebMapTest/GPServer/Export%20Web%20Map';
+        return 'http://localhost:6080/arcgis/rest/services/DelDotWebViewer/ExportWebMapTest/GPServer/Export%20Web%20Map';
     },
 
     getReportExportPathUrl: function () {
-        //return 'http://vm-city102/';
-        return 'https://newarkgis.kci.com/';
+        return 'http://localhost/';
     },
 
     getBaseUrl: function () {
